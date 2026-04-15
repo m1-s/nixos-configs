c10095f29d361a5850ea31ce6a97a4ad752d6b11
Michael Schneider <michael@m1-s.com>
2026-04-15 17:23:15 +0100

    enable uid-range system feature

--- nixos-modules/experimental.nix
+++ nixos-modules/experimental.nix
@@ -3,6 +3,7 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    settings.system-features = [ "nixos-test" "benchmark" "big-parallel" "kvm" "uid-range" ];
     # nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
     # pin nix flake registry, to avoid downloading the latest all the time
     # registry.nixpkgs.flake = inputs.nixpkgs;
